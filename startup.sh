4c9e00b9d2f3e7e0860f15e1445fde2e14ca85ab
cohrint <cohrint@tars.cpr-unassigned>
2021-11-11 14:41:53 -0500

    Cleaned up launch and startup file

--- startup.sh
+++ startup.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-
-source /opt/ros/indigo/setup.bash
+# Source necessary setup files for ROS
+source /opt/ros/noetic/setup.bash
 source /home/cohrint/catkin_ws/devel/setup.bash
-export ROBOT_NAME=case
-roslaunch jackal_base base.launch
 
+# Export robot name
+export ROBOT_NAME=tars
 
+# Launch ros master depending on specified indoor/outdoor/aspen
+if [ $1 == 'indoor' ]
+then
+    roslaunch cohrint_jackal_bringup jackal_indoor.launch
+elif [ $1 == 'aspen' ]
+then
+    roslaunch cohrint_jackal_bringup jackal_aspen.launch
+else
+    roslaunch cohrint_jackal_bringup jackal_outdoor.launch
+fi
